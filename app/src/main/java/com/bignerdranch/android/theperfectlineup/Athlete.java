8bb3510f587b1dd9519470031e2ed96305801d19
Maryann O'Connell <maryann_oconnell@uml.edu>
2016-01-23 23:17:19 -0500

    new obj files

---
+++ app/src/main/java/com/bignerdranch/android/theperfectlineup/Athlete.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.theperfectlineup;
+
+/**
+ * Created by Maryann on 1/23/16.
+ */
+public class Athlete {
+    String firstName, lastName;
+
+}
---
+++ app/src/main/java/com/bignerdranch/android/theperfectlineup/Rower.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.theperfectlineup;
+
+/**
+ * Created by Maryann on 1/23/16.
+ */
+public class Rower {
+
+    double twokTime, getSixkTime, weight;
+    boolean isCoxswain, isStarboard, isPort;
+
+    Rower(String firstName, String lastName )
+
+
+}
