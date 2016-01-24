be87f1e5153423ada91d03caf792af302f428960
Maryann O'Connell <maryann_oconnell@uml.edu>
2016-01-23 19:34:14 -0500

    merged file

--- app/src/main/java/com/bignerdranch/android/theperfectlineup/NewCoachRegistration.java
+++ app/src/main/java/com/bignerdranch/android/theperfectlineup/NewCoachRegistration.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class NewCoachRegistration extends AppCompatActivity implements View.OnClickListener {
+public class NewCoachRegistration extends AppCompatActivity {
     private Button submitBtn;
 
     @Override
@@ -16,39 +16,7 @@ public class NewCoachRegistration extends AppCompatActivity implements View.OnCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_coach_registration);
         submitBtn = (Button) findViewById(R.id.button_submit);
-        submitRegButton.setOnClickListener(new View.OnClickListener()) {
-            public void onClick(View v) {
-               /* Intent i = new Intent();
-                i.setComponent(new ComponentName("com.android.contacts", "com.android.contacts.DialtactsContactsEntryActivity"));
-                i.setAction("android.intent.action.MAIN");
-                i.addCategory("android.intent.category.LAUNCHER");
-                i.addCategory("android.intent.category.DEFAULT");
-                v.getContext().startActivity(i);*/
-                setContentView(R.layout.activity_login);
-            }
 
-        }
     }
 }
-   /* public void onClick(View view) {
-        Intent myIntent = new Intent(this, LoginActivity.class);
-        startActivity(myIntent);
-    }
-    */
-
-
-    /*
-    buttonItem.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        Intent i = new Intent();
-        i.setComponent(new ComponentName("com.android.contacts", "com.android.contacts.DialtactsContactsEntryActivity"));
-        i.setAction("android.intent.action.MAIN");
-        i.addCategory("android.intent.category.LAUNCHER");
-        i.addCategory("android.intent.category.DEFAULT");
-        v.getContext().startActivity(i);
-    }
-
-     */
-
 
