b599c200d2374ddd99a72c19a43edfef5ebf9d2b
Gina <ginagonzalezroundey@gmail.com>
2016-01-24 05:14:46 -0500

    drawables

--- app/src/main/java/com/bignerdranch/android/theperfectlineup/home.java
+++ app/src/main/java/com/bignerdranch/android/theperfectlineup/home.java
@@ -16,12 +16,15 @@ import android.widget.LinearLayout;
 public class home extends AppCompatActivity {
 
     private int numOfRowers = 8;        //TODO fill with roster data
-    private TextView[] rowers = new TextView[numOfRowers];  //TODO add textView to rower obj
+    private TextView[] rowers = new TextView[numOfRowers];  //TODO add textView to portrower obj
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+//        TextView first = (TextView) findViewById(R.id.firstRower);
+//        first.setOnTouchListener(new ChoiceTouchListener());
 
         addRowers();
 
@@ -30,16 +33,34 @@ public class home extends AppCompatActivity {
 
     public void addRowers()
     {
-        LinearLayout layout = (LinearLayout)findViewById(R.id.rowerLinearLayout);
+        //TODO call either port or starboard method by id
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.portRowerLinearLayout);
         layout.setScrollContainer(true);
 
         for (int i = 0; i < numOfRowers; i++)
         {
-            rowers[i] = new TextView(this);     //TODO initialize in rower object
+            rowers[i] = new TextView(this);     //TODO initialize in portrower object
             rowers[i].setText("Rower " + i);    //TODO change i to name attribute
-            rowers[i].setBackgroundResource(R.drawable.rower);
+            rowers[i].setBackgroundResource(R.drawable.portrower);
+            rowers[i].setWidth(450);
+            rowers[i].setHeight(250);
             rowers[i].setOnTouchListener(new ChoiceTouchListener());
             layout.addView(rowers[i]);
+        }
+
+        LinearLayout layout2 = (LinearLayout)findViewById(R.id.starboardRowerLinearLayout);
+        layout2.setScrollContainer(true);
+
+        for (int i = 0; i < numOfRowers; i++)
+        {
+            rowers[i] = new TextView(this);     //TODO initialize in portrower object
+            rowers[i].setText("Rower " + i);    //TODO change i to name attribute
+            rowers[i].setBackgroundResource(R.drawable.starboardrower);
+            rowers[i].setWidth(450);
+            rowers[i].setHeight(250);
+            rowers[i].setOnTouchListener(new ChoiceTouchListener());
+            layout2.addView(rowers[i]);
         }
     }
 
@@ -47,13 +68,32 @@ public class home extends AppCompatActivity {
     {
         LinearLayout layout = (LinearLayout)findViewById(R.id.boatLinearLayout);
         layout.setScrollContainer(true);
-        for (int i = 1; i < 9; i++)
+        for (int i = 0; i < 9; i++)
         {
             TextView temp = new TextView(this);
-            temp.setText("" + i);
-            temp.setBackgroundResource(R.drawable.choice);
-            temp.setOnDragListener(new ChoiceDragListener());
-            layout.addView(temp);
+            if(i == 0){
+                temp.setText("Coxswain");
+                temp.setBackgroundResource(R.drawable.cox);
+                temp.setWidth(450);
+                temp.setHeight(200);
+                temp.setOnDragListener(new ChoiceDragListener());
+                layout.addView(temp);
+            }
+            else {
+                if (i % 2 == 0) {
+                    temp.setText("" + i);
+                    temp.setBackgroundResource(R.drawable.starboardrower_shadow);
+                    temp.setHeight(250);
+                    temp.setOnDragListener(new ChoiceDragListener());
+                    layout.addView(temp);
+                } else {
+                    temp.setText("" + i);
+                    temp.setBackgroundResource(R.drawable.portrower_shadow);
+                    temp.setHeight(250);
+                    temp.setOnDragListener(new ChoiceDragListener());
+                    layout.addView(temp);
+                }
+            }
         }
     }
 
@@ -89,14 +129,23 @@ public class home extends AppCompatActivity {
                 case DragEvent.ACTION_DROP:
                     //handle the dragged view being dropped over a target view
                     View view = (View) event.getLocalState();
-                    //stop displaying the view where it was before it was dragged
-                    view.setVisibility(View.INVISIBLE);
+                    //grey out oar
+                    view.setAlpha((float)0.5);
                     //view dragged item is being dropped on
                     TextView dropTarget = (TextView) v;
                     //view being dragged and dropped
                     TextView dropped = (TextView) view;
                     //update the text in the target view to reflect the data being dropped
                     dropTarget.setText(dropped.getText());
+//                    if(Integer.parseInt((String)dropped.getText()) % 2 == 0){
+//                        dropTarget.setBackgroundResource(R.drawable.starboardrower);
+//                    }
+//                    else {
+                        dropTarget.setBackgroundResource(R.drawable.portrower);
+           //         }
+                    dropTarget.setHeight(202);
+                    dropTarget.setWidth(20);
+                    dropTarget.setPaddingRelative(0, 0, 0, 0);
                     //make it bold to highlight the fact that an item has been dropped
                     dropTarget.setTypeface(Typeface.DEFAULT_BOLD);
                     //if an item has already been dropped here, there will be a tag
